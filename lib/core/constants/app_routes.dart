a10bdcbf6e2f67fdff8e27458f072c27aa14579c
Matheus Reis <matheus.reis@modalgr.com.br>
2025-11-05 00:39:49 -0300

    init: initial commit

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:im_mottu_mobile/views/home_page.dart';
+
+Map<String, WidgetBuilder> appRoutes = {
+  '/': (_) => const HomePage(),
+};
---
+++ lib/core/constants/contants.dart
@@ -0,0 +1,4 @@
+export 'api.dart';
+export 'app_colors.dart';
+export 'app_routes.dart';
+export 'app_text.dart';
